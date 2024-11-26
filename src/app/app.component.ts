6fb65791c1d0958cabc6e87c1edd2e9eeee5c3d1
Sr-samir <samirricardo22@gmail.com>
2024-11-26 13:17:33 -0300

    atualizaçao-home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
 import { SectionComponent } from './components/section/section.component';
+import { CarrosselComponent } from './components/carrossel/carrossel.component';
 
 
 
@@ -11,7 +12,7 @@ import { SectionComponent } from './components/section/section.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavComponent, SectionComponent],
+  imports: [RouterOutlet, HeaderComponent, NavComponent, SectionComponent, CarrosselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
